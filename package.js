42570137799790e01c6cb3ee5ff5e4fa39d6ddad
Radosław Miernik <radekmie@gmail.com>
2015-08-25 15:37:47 +0200

    Removed time from END

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: 'vazco:universe-autoform-scheduler',
     summary: 'Custom "afUniverseScheduler" input type for AutoForm',
-    version: '0.1.0',
+    version: '0.1.1',
     git: 'https://github.com/vazco/meteor-universe-autoform-scheduler.git'
 });
 
@@ -15,8 +15,8 @@ Package.onUse(function (api) {
     api.use('underscore');
     api.use('check');
     api.use('aldeed:autoform@4.0.0');
-    // api.use('rajit:bootstrap3-datepicker@1.1.1');
-    api.use('tsega:bootstrap3-datetimepicker@4.14.30_4'),
+    api.use('rajit:bootstrap3-datepicker@1.1.1');
+    api.use('tsega:bootstrap3-datetimepicker@4.14.30_4');
     api.use('momentjs:moment@2.9.0');
     api.use('vazco:universe-utilities@1.0.7');
     api.use('reactive-dict@1.1.0');
